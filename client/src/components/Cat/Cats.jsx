6748e69f13f97a14b6e480a2cc840494bf1a6393
JavierCollipal <jcollipal1212@gmail.com>
2020-12-15 16:02:58 -0300

    saving work pre nest testing

--- client/src/components/Cat/Cats.jsx
+++ client/src/components/Cat/Cats.jsx
@@ -2,13 +2,21 @@ import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CatForm from './CatForm';
 import CatList from './CatList';
-import { defaultCatImg } from '../../utils/mock/cats';
+import {defaultCatImg, mockCatData} from '../../utils/mock/cats';
 import Skeleton from '@material-ui/lab/Skeleton';
 
-const Cats = ({ cats, loading, error, addCat, fetchCats, deleteCat, updateCat }) => {
+const Cats = () => {
+  const [cats, setCats] = useState(mockCatData);
+  const [loading, setLoading]  = useState();
+  const [error, setError] = useState();
 	const [formData, setFormData] = useState();
 	const [dialog, setDialog] = useState(false);
 	const [updateMode, setUpdateMode] = useState(false);
+
+	const fetchCats = () => {};
+  const addCat = cat => {};
+  const deleteCat = catId => {};
+  const updateCat = (catData,catId) => {};
 
 	const handleUpdate = cat => {
 		setFormData(cat);
@@ -25,39 +33,13 @@ const Cats = ({ cats, loading, error, addCat, fetchCats, deleteCat, updateCat })
 		setUpdateMode(false);
 	};
 	const handleFormSubmit = payload => {
-		//arreglo momentaneo para el error de redux-form(initialValues no esta pasando la id si es update)
-		//arreglo momentaneo para el caso de create, asignar parametros adentro del reducer estaba mal ya q			<form>
-    // 					<Field
-    // 						name="name"
-    // 						component={CustomField}
-    // 						placeholder="Nombre"
-    // 						title="Nombre:"
-    // 						type="text"
-    // 					/>
-    // 					<Field
-    // 						name="age"
-    // 						component={CustomField}
-    // 						placeholder="Edad"
-    // 						title="Edad"
-    // 						type="number"
-    // 					/>
-    // 					<Field
-    // 						name="breed"
-    // 						component={CustomField}
-    // 						placeholder="Raza"
-    // 						title="Raza"
-    // 						type="text"
-    // 					/>
-    // 				</form>
-    // 		ue no estaba corriendo bien el test de add por esto mismo
-		//de todas maneras con redux thunk en add se solucionaria el problema de asignar estos valores en esta funcion
-		if (updateMode) payload = { ...payload, id: formData._id };
-		else payload = { ...payload, imageUrl: defaultCatImg };
-		setDialog(false);
-		updateMode ? updateCat(payload) : addCat(payload);
+	if (updateMode) payload = { ...payload, id: formData._id };
+	else payload = { ...payload, imageUrl: defaultCatImg };
+	setDialog(false);
+	updateMode ? updateCat(payload) : addCat(payload);
 	};
 
-	//simulamos componentDidMount con un arreglo en blanco como segundo parametro,
+  //simulamos componentDidMount con un arreglo en blanco como segundo parametro,
 	//en este caso lleva la funcion fetchCat por recomendación del linter
 
 	return (
